de8ae38bf937e8e2701e455328f9da6766304628
Edvige Di Salvo <edvigedis@google.com>
2024-12-02 17:21:06 +0100

    routes and CORS are done

--- server/app.js
+++ server/app.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const dataCohorts = require("./cohorts.json");
+const dataStudents = require("./students.json");
 const PORT = 5005;
+const cors = require("cors");
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
 
-
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
-
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
@@ -20,7 +21,7 @@ app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(cors({ origin: ["http://localhost:5173"] }));
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
@@ -28,7 +29,12 @@ app.use(cookieParser());
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
-
+app.get("/api/cohorts", (req, res) => {
+  res.json(dataCohorts);
+});
+app.get("/api/students", (req, res) => {
+  res.json(dataStudents);
+});
 
 // START SERVER
 app.listen(PORT, () => {
