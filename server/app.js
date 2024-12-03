43becf6b2182ed68a2e9880971627f9e72d71893
Pandau <pandau.ting@gmail.com>
2024-12-03 09:31:15 +0100

    some refactoring

--- server/app.js
+++ server/app.js
@@ -1,42 +1,43 @@
-const express = require("express");
-const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
-const dataCohorts = require("./cohorts.json");
-const dataStudents = require("./students.json");
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
 const PORT = 5005;
-const cors = require("cors");
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
+const dataCohorts = require('./cohorts.json');
+const dataStudents = require('./students.json');
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
-// ...
+app.use(cors({ origin: ['http://localhost:5173'] }));
+
 app.use(express.json());
-app.use(morgan("dev"));
-app.use(express.static("public"));
+app.use(morgan('dev'));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors({ origin: ["http://localhost:5173"] }));
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
-// ...
-app.get("/docs", (req, res) => {
-  res.sendFile(__dirname + "/views/docs.html");
+app.get('/docs', (req, res) => {
+	res.sendFile(__dirname + '/views/docs.html');
 });
-app.get("/api/cohorts", (req, res) => {
-  res.json(dataCohorts);
+
+app.get('/api/cohorts', (req, res) => {
+	res.json(dataCohorts);
 });
-app.get("/api/students", (req, res) => {
-  res.json(dataStudents);
+
+app.get('/api/students', (req, res) => {
+	res.json(dataStudents);
 });
 
 // START SERVER
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+	console.log(`Server listening on port ${PORT}`);
 });
