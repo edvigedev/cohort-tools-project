06509400504f7a94bcfc206a68b411ba4b0d243c
Bruno Frasan <brunofrancco@gmail.com>
2024-12-04 15:16:36 +0000

    done day 2

--- server/app.js
+++ server/app.js
@@ -1,70 +1,70 @@
-const express = require('express');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+const express = require('express')
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
-const mongoose = require('mongoose');
-const Student = require('./models/Student.js');
-const Cohort = require('./models/Cohort.js');
+const mongoose = require('mongoose')
+const Student = require('./models/Student.js')
+const Cohort = require('./models/Cohort.js')
 
-const config = require('./config.js');
-
-// LOAD ENVIRONMENT VARIABLES
-const PORT = config.PORT;
-const MONGO_URI = config.MONGO_URI;
+const PORT = 5005
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-const dataCohorts = require('./cohorts.json');
-const dataStudents = require('./students.json');
+const dataCohorts = require('./cohorts.json')
+const dataStudents = require('./students.json')
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
-const app = express();
+const app = express()
 
-// DATABASE
-// MongoDB debugging logs
-mongoose.set('debug', true);
+/*************
+ * MONGODB
+ *************/
+// MongoDB connection string
+const MONGO_URI = 'mongodb://localhost:27017/cohort-tools-api'
+
+mongoose.set('debug', true)
 
 // Connect to MongoDB
 mongoose
 	.connect(MONGO_URI)
 	.then(() => console.log('MongoDB connected'))
-	.catch(err => console.error('MongoDB connection error:', err));
+	.catch((err) => console.error('MongoDB connection error:', err))
 
 // Test Connection
 app.get('/test', async (req, res) => {
-	const student = await Student.find({ firstName: 'Grace' });
-	const cohort = await Cohort.find({ campus: 'Paris' });
-	console.log('Student:', student);
-	console.log('Cohort:', cohort);
-	res.status(200).json(cohort);
-});
+	const student = await Student.find({ firstName: 'Grace' })
+	const cohort = await Cohort.find({ campus: 'Paris' })
+	console.log('Student:', student)
+	console.log('Cohort:', cohort)
+	res.status(200).json(cohort)
+})
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
-app.use(cors({ origin: ['http://localhost:5173'] }));
+app.use(cors({ origin: ['http://localhost:5173'] }))
 
-app.use(express.json());
-app.use(morgan('dev'));
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 app.get('/docs', (req, res) => {
-	res.sendFile(__dirname + '/views/docs.html');
-});
+	res.sendFile(__dirname + '/views/docs.html')
+})
 
 app.get('/api/cohorts', (req, res) => {
-	res.json(dataCohorts);
-});
+	res.json(dataCohorts)
+})
 
 app.get('/api/students', (req, res) => {
-	res.json(dataStudents);
-});
+	res.json(dataStudents)
+})
 
 // START SERVER
 app.listen(PORT, () => {
-	console.log(`Server listening on port ${PORT}`);
-});
+	console.log(`Server listening on port ${PORT}`)
+})
